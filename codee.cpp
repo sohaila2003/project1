3a8838cb92f0746db7681e769d100ac364d1e268
progect1 <sohaila.202501069@compit.aun.edu.eg>
2021-10-19 13:42:37 +0200

    first commit

---
+++ codee.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void main ()
+{
+cout << "enter two numbers";
+int num1, num2;
+cin>> num1;
+cin>> num2;
+int sum;
+sum = num1 + num2;
+cout << sum << endl;
+
+system("pause");
+}
